957ecf30eb1dbcbdc0ee42c9ada5d4d6f0f22f79
prinako <nyankonton122@gmail.com>
2024-02-27 23:28:02 -0300

    configuração do estruturar do app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:libras/main.dart';
+//import 'package:libras/main.dart';
+import 'package:libras/app/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
