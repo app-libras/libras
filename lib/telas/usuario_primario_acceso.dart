95d5ee963e9ea9cbfa38c0671726e1ca4bb5e0c3
Kwamesmf <128297525+Kwamesmf@users.noreply.github.com>
2024-03-11 15:25:32 -0300

    colors
    added colors and removed  the text and picture of libras which used to show on the screen to see how it appears on the app

--- lib/telas/usuario_primario_acceso.dart
+++ lib/telas/usuario_primario_acceso.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class UsuarioAcceso extends StatefulWidget {
   const UsuarioAcceso({super.key});
@@ -11,100 +12,108 @@ class _UsuarioAccesoState extends State<UsuarioAcceso> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Column(
-                  children: <Widget>[
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(244, 4, 245, 149),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     const Column(
+              //       children: <Widget>[
+              //         Text(
+              //           'Seja Bem Vindo',
+              //           style: TextStyle(
+              //             fontSize: 25,
+              //             fontWeight: FontWeight.w500,
+              //           ),
+              //         ),
+              //         SizedBox(
+              //           height: 5,
+              //         ),
+              //         Row(
+              //           children: <Widget>[
+              //             Text(
+              //               'ao',
+              //               style: TextStyle(
+              //                 fontSize: 25,
+              //                 fontStyle: FontStyle.italic,
+              //               ),
+              //             ),
+              //             Text(
+              //               ' LIBRAR.',
+              //               style: TextStyle(
+              //                 fontSize: 30,
+              //                 fontWeight: FontWeight.bold,
+              //               ),
+              //             ),
+              //           ],
+              //         ),
+              //       ],
+              //     ),
+              //     Image.asset(
+              //       'assets/images/sign.png',
+              //       height: 100,
+              //     ),
+              //   ],
+              // ),
+              // const SizedBox(
+              //   height: 5,
+              // ),
+              GestureDetector(
+                onTap: () {},
+                child: const Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    CircleAvatar(
+                      radius: 80,
+                      backgroundColor: Color.fromARGB(255, 181, 190, 186),
+                    ),
                     Text(
-                      'Seja Bem Vindo',
+                      'avatar',
                       style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w500,
+                        color: Color.fromARGB(246, 28, 27, 27),
+                        fontSize: 20,
+                        decoration: TextDecoration.underline,
                       ),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          'ao',
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontStyle: FontStyle.italic,
-                          ),
-                        ),
-                        Text(
-                          ' LIBRAR.',
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
+                    )
                   ],
                 ),
-                Image.asset(
-                  'assets/images/sign.png',
-                  height: 100,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            const Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                CircleAvatar(
-                  radius: 40,
-                  backgroundImage: null,
-                  backgroundColor: Colors.blueAccent,
-                ),
-                Text(
-                  'avatar',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    decoration: TextDecoration.underline,
-                    backgroundColor: Colors.transparent,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Center(
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: "Seu Nome:",
-                  hintText: 'Digite seu nome',
-                  hintStyle: const TextStyle(color: Colors.black),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-                textCapitalization: TextCapitalization.words,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const ElevatedButton(
-              onPressed: null,
-              child: Text('Iniciar'),
-            )
-          ],
+              const SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "Seu Nome:",
+                    hintText: 'Digite seu nome',
+                    hintStyle: const TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  textCapitalization: TextCapitalization.words,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+
+              const ElevatedButton(
+                onPressed: null,
+                child: Text('Entrar'),
+              )
+            ],
+          ),
         ),
-      ),
-    );
+        bottomNavigationBar: const BottomAppBar(
+          color: Color.fromARGB(244, 4, 245, 149),
+          height: 50,
+        ));
   }
 }
