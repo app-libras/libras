ce0f0ac4ef384bcb86a0629bb5fc06bb5fb3b63d
Kwamesmf <128297525+Kwamesmf@users.noreply.github.com>
2024-03-12 12:27:50 -0300

    update on screen
    new button near profile picture and removed colors

--- lib/telas/usuario_primario_acceso.dart
+++ lib/telas/usuario_primario_acceso.dart
@@ -6,133 +6,118 @@ class UsuarioAcceso extends StatefulWidget {
   @override
   State<UsuarioAcceso> createState() => _UsuarioAccesoState();
 }
-// class _ProfileScreenState extends State<ProfileScreen> {
-//   String selectedAvatar = 'assets/avatar1.png'; // Initial avatar path
-
-// // class _ProfileScreenState extends State<ProfileScreen> {
-// String selectedAvatar = 'assets/images/do-utilizador.png';
-
-// List<String> avatars = [
-//   'assets/images/garoto (1).png',
-//   'assets/images/monstro.png',
-// ];
-
-// // Function to update the selected avatar
-// void _updateAvatar(String newAvatar) {
-//   setState(() {
-//     selectedAvatar = newAvatar;
-//   });
-// }
 
 class _UsuarioAccesoState extends State<UsuarioAcceso> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(244, 4, 245, 149),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     const Column(
-              //       children: <Widget>[
-              //         Text(
-              //           'Seja Bem Vindo',
-              //           style: TextStyle(
-              //             fontSize: 25,
-              //             fontWeight: FontWeight.w500,
-              //           ),
-              //         ),
-              //         SizedBox(
-              //           height: 5,
-              //         ),
-              //         Row(
-              //           children: <Widget>[
-              //             Text(
-              //               'ao',
-              //               style: TextStyle(
-              //                 fontSize: 25,
-              //                 fontStyle: FontStyle.italic,
-              //               ),
-              //             ),
-              //             Text(
-              //               ' LIBRAR.',
-              //               style: TextStyle(
-              //                 fontSize: 30,
-              //                 fontWeight: FontWeight.bold,
-              //               ),
-              //             ),
-              //           ],
-              //         ),
-              //       ],
-              //     ),
-              //     Image.asset(
-              //       'assets/images/sign.png',
-              //       height: 100,
-              //     ),
-              //   ],
-              // ),
-              // const SizedBox(
-              //   height: 5,
-              // ),
-              GestureDetector(
-                onTap: () {
-                  // _updateAvatar(avatar);
-                },
-                child: const Stack(
-                  alignment: Alignment.bottomCenter,
-                  children: [
-                    CircleAvatar(
-                      radius: 80,
-                      // backgroundImage: AssetImage(Sele),
-                      // backgroundColor: Color.fromARGB(255, 181, 190, 186),
-                    ),
-                    Text(
-                      'avatar',
-                      style: TextStyle(
-                        color: Color.fromARGB(246, 28, 27, 27),
-                        fontSize: 20,
-                        decoration: TextDecoration.underline,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Seu Nome:",
-                    hintText: 'Digite seu nome',
-                    hintStyle: const TextStyle(color: Colors.black),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
+      // appBar: AppBar(
+      //   backgroundColor: const Color.fromARGB(244, 4, 245, 149),
+      // ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: [
+            //     const Column(
+            //       children: <Widget>[
+            //         Text(
+            //           'Seja Bem Vindo',
+            //           style: TextStyle(
+            //             fontSize: 25,
+            //             fontWeight: FontWeight.w500,
+            //           ),
+            //         ),
+            //         SizedBox(
+            //           height: 5,
+            //         ),
+            //         Row(
+            //           children: <Widget>[
+            //             Text(
+            //               'ao',
+            //               style: TextStyle(
+            //                 fontSize: 25,
+            //                 fontStyle: FontStyle.italic,
+            //               ),
+            //             ),
+            //             Text(
+            //               ' LIBRAR.',
+            //               style: TextStyle(
+            //                 fontSize: 30,
+            //                 fontWeight: FontWeight.bold,
+            //               ),
+            //             ),
+            //           ],
+            //         ),
+            //       ],
+            //     ),
+            //     Image.asset(
+            //       'assets/images/sign.png',
+            //       height: 100,
+            //     ),
+            //   ],
+            // ),
+            // const SizedBox(
+            //   height: 5,
+            // ),
+            GestureDetector(
+              onTap: () {},
+              child: const Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  CircleAvatar(
+                    radius: 80,
+                    child: Icon(
+                      Icons.person,
+                      size: 30,
                     ),
                   ),
-                  textCapitalization: TextCapitalization.words,
+                  Positioned(
+                    bottom: 10,
+                    left: 140,
+                    child: Icon(
+                      Icons.add_circle,
+                      color: Colors.black,
+                      size: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Center(
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Seu Nome:",
+                  hintText: 'Digite seu nome',
+                  hintStyle: const TextStyle(color: Colors.black),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
+                textCapitalization: TextCapitalization.words,
               ),
-              const SizedBox(
-                height: 20,
-              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
 
-              const ElevatedButton(
-                onPressed: null,
-                child: Text('Entrar'),
-              )
-            ],
-          ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Entrar'),
+            )
+          ],
         ),
-        bottomNavigationBar: const BottomAppBar(
-          color: Color.fromARGB(244, 4, 245, 149),
-          height: 50,
-        ));
+      ),
+      // bottomNavigationBar: const BottomAppBar(
+      //   color: Color.fromARGB(244, 4, 245, 149),
+      //   height: 50,
+      // ));
+    );
   }
 }
