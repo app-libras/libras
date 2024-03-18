3a55952529a5b07576aa2ae2059bd2aa1247ab49
Kwamesmf <128297525+Kwamesmf@users.noreply.github.com>
2024-03-18 13:04:25 -0300

    avatar screen
    will create the  avatar screen soon

---
+++ lib/telas/avatar.dart
@@ -0,0 +1,8 @@
+// import 'package:flutter/material.dart';
+
+// class Avatar extends StatefulWidget {
+//   const Avatar({super.key});
+
+//   @override
+//   _AvatarState createState() => _AvatarState();
+// }
