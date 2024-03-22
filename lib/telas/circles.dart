2d2ac4f818ac5cfdc9f72b3b4aaee66ebd342ff5
Kwamesmf <128297525+Kwamesmf@users.noreply.github.com>
2024-03-22 18:25:11 -0300

    Update circles.dart

--- lib/telas/circles.dart
+++ lib/telas/circles.dart
@@ -2,38 +2,32 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class CirclesScreen extends StatefulWidget {
-  const CirclesScreen ({super.key});
-  
+  const CirclesScreen({super.key});
+
   @override
   _CirclesScreenState createState() => _CirclesScreenState();
 }
 
-
-class _CirclesScreenState extends State<CirclesScreen>{
-@override 
-Widget build(BuildContext context){
-  return Scaffold(
-    appBar: AppBar(
+class _CirclesScreenState extends State<CirclesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
       backgroundColor: Colors.greenAccent,
       bottom: const PreferredSize(
         preferredSize: Size.fromHeight(10),
-      child: Padding(
-        padding: EdgeInsets.only(left:25),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        CircularProgressIndicator(),
-      SizedBox(width: 10,),
-      ],
-      )),
+        child: Padding(
+            padding: EdgeInsets.only(left: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                CircularProgressIndicator(),
+                SizedBox(
+                  width: 10,
+                ),
+              ],
+            )),
       ),
-      
-
-
-
-
-
-    )
-    );
-}
+    ));
+  }
 }
