a94c63e3dc4e2fb653f7b71bd9385b8f17cdf65f
Kwamesmf <128297525+Kwamesmf@users.noreply.github.com>
2024-03-22 19:20:00 -0300

    progress screen
    working on  progress screen

--- lib/telas/circles.dart
+++ lib/telas/circles.dart
@@ -12,22 +12,38 @@ class _CirclesScreenState extends State<CirclesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Colors.greenAccent,
-      bottom: const PreferredSize(
-        preferredSize: Size.fromHeight(10),
-        child: Padding(
-            padding: EdgeInsets.only(left: 25),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                CircularProgressIndicator(),
-                SizedBox(
-                  width: 10,
-                ),
-              ],
-            )),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(244, 4, 245, 149),
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(10),
+          child: Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Stack(children: [
+                    CircularProgressIndicator(),
+                    Icon(
+                      Icons.person,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                  ]),
+                ],
+              )),
+        ),
       ),
-    ));
+      body: const Center(
+        child: CustomPaint(
+          size: Size(300, 300),
+          painter: null,
+        ),
+      ),
+      bottomNavigationBar: const BottomAppBar(
+        color: Color.fromARGB(244, 4, 245, 149),
+        height: 50,
+      ),
+    );
   }
 }
