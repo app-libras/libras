25e15a12ec0f815f54dd5a3314548918ace05185
prince nyarko <prinako.dev+github@gmail.com>
2025-04-19 15:23:58 -0300

    added database SQLite

---
+++ lib/core/constants/database_tables_name_constants.dart
@@ -0,0 +1,9 @@
+class DatabaseTablesName {
+  static const String user = 'user';
+  static const String question = 'question';
+  static const String score = 'score';
+  static const String answer = 'answer';
+  static const String materia = 'materia';
+  static const String aula = 'aula';
+  static const String materiaAula = 'materia_aula';
+}
