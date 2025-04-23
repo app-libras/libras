effd39c940d087b74de52772978a2a1d23de3ca4
prince nyarko <prinako.dev+github@gmail.com>
2025-04-22 21:07:04 -0300

    icons

---
+++ lib/core/fonts/books_icons.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+class BooksIcons {
+  BooksIcons._();
+
+  static const _kFontFam = 'MyFlutterApp';
+  static const String? _kFontPkg = null;
+
+  static const IconData book = IconData(
+    0xe800,
+    fontFamily: _kFontFam,
+    fontPackage: _kFontPkg,
+  );
+  static const IconData book_open_1 = IconData(
+    0xe801,
+    fontFamily: _kFontFam,
+    fontPackage: _kFontPkg,
+  );
+}
